9a2dbce47df45c6c748ab0e7a666dd7629dff326
Peter-JC-Wu <peterjcwu@gmail.com>
2023-05-06 15:05:29 -0400

    updated js code with addEventListener

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-// your code here
+document.getElementById('submit').addEventListener('click', function () {
+  var name = document.getElementById('name').value;
+  var textMessage = document.getElementById('message').value;
+
+  var postsDiv = document.querySelector('.posts');
+
+  var newPostDiv = document.createElement('div');
+
+  var newPostTextMessage = document.createElement('p');
+  var newPostTextMessageNode = document.createTextNode(textMessage);
+  newPostTextMessage.appendChild(newPostTextMessageNode);
+
+  var newPostNameMessage = document.createElement('p');
+  var newPostNameMessageNode = document.createTextNode('Posted by: ' + name);
+  newPostNameMessage.appendChild(newPostNameMessageNode);
+
+  var newPostHR = document.createElement('hr');
+
+  newPostDiv.append(newPostTextMessage);
+  newPostDiv.append(newPostNameMessage);
+  newPostDiv.append(newPostHR);
+
+  postsDiv.append(newPostDiv);
+})
